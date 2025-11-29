f765e740d1420be4fa51aee51e819aa17b6f2f8e
ctganter <cg6149@princeton.edu>
2025-11-29 03:11:31 -0500

    Continued integrating cache and cycle

--- project2/src/cycle.cpp
+++ project2/src/cycle.cpp
@@ -56,9 +56,9 @@ Status initSimulator(CacheConfig& iCacheConfig, CacheConfig& dCacheConfig, Memor
 // cache
 
 // keep track of the number of cycles stall is applied
-static int stallCyclesCount = 0;
+static int loadStallCount = 0;
 static int iCacheStallCycles = 0;
-
+static int dCacheStallCycles = 0;
 
 Status runCycles(uint64_t cycles) {
     uint64_t count = 0;
@@ -87,237 +87,156 @@ Status runCycles(uint64_t cycles) {
         pipelineInfo.wbInst = simulator->simWB(memPrev);
     
     // MEM
-        pipelineInfo.memInst = simulator->simMEM(exPrev);
         
-        bool StallID    = false;
-        bool BubbleEx   = false;
+    // D-Cache Stall
 
-        // Hazard Detection
-        
-        bool idPrevisBranch = (idPrev.opcode == OP_BRANCH) || 
-                        (idPrev.opcode == OP_JAL) ||
-                        (idPrev.opcode == OP_JALR);
-        
-        if(stallCyclesCount > 0){
-            StallID = true;
-            BubbleEx = true;
-            stallCyclesCount --;
+        // Checks for a dCache Miss
+        // QUESTION: Do we have to implement zero register nullification?
+        bool dCacheStall = exPrev.readsMem && !dCache->access(exPrev.memAddress, CACHE_READ) ||
+                            exPrev.writesMem && !dCache->access(exPrev.memAddress, CACHE_WRITE);
+
+        // Case: Currently stalling for dCache miss latency
+        if(dCacheStallCycles != 0){
+            pipelineInfo.memInst = nop(BUBBLE);
+            dCacheStallCycles--;
         }
-        else{
-        // Handle load-use stalls
-            if((idPrev.rs1 == exPrev.rd ||
-                idPrev.rs2 == exPrev.rd) && 
-                exPrev.readsMem && exPrev.writesRd
-                && exPrev.rd != 0 && !idPrevisBranch)
-            {
-                BubbleEx = true;
-            }
-
-            // Handle arith-branch
-            // CHANGE TO CHECK FOR BEQ/BNE, etc
-            if(exPrev.doesArithLogic && exPrev.writesRd && exPrev.rd != 0
-            && idPrevisBranch && 
-            (idPrev.rs1 == exPrev.rd || idPrev.rs2 == exPrev.rd))
-            {            
-                StallID = true;
-            }
-
-            // Handle load-branch
-            if(exPrev.readsMem && exPrev.writesRd && exPrev.rd != 0
-                && idPrevisBranch && (idPrev.rs1 == exPrev.rd ||
-                idPrev.rs2 == exPrev.rd))
-            {
-                StallID = true;
-                BubbleEx = true;
-                stallCyclesCount = 1;
-            }
+        // Case: dCache miss on current cycle
+        else if (dCacheStall){
+            pipelineInfo.memInst = nop(BUBBLE);
+            dCacheStallCycles = dCache->config->missLatency;
         }
-
-        if(BubbleEx) {
-            // insert bubble
-            pipelineInfo.exInst = nop(BUBBLE);
-        } else {
-
-            // normal case
-            // Do FORWARDING between prevMem and Ex and/or prevEx and Ex
-
-            // rs1 forwarding - these can be put in helper functions!!
-            if (idPrev.readsRs1 && idPrev.rs1 != 0) {
-                // From EX/MEM (exPrev) - ALU result is ready here
-                if (exPrev.writesRd && exPrev.rd == idPrev.rs1 &&
-                    exPrev.doesArithLogic) {
-                    idPrev.op1Val = exPrev.arithResult;
-                }
-                // From MEM  (memPrev) - load result is ready here
-                if (memPrev.writesRd && memPrev.rd == idPrev.rs1) {
-                    if (memPrev.readsMem) {
-                        idPrev.op1Val = memPrev.memResult;
-                    } else if (memPrev.doesArithLogic) {
-                        idPrev.op1Val = memPrev.arithResult;
-                    }
-                }
-                // From WB stage (just wrote back this cycle)- SORRY,
-                // I deleted it, we dont need it
-            
-            }
-            // rs2 forwarding
-            if (idPrev.readsRs2 && idPrev.rs2 != 0) {
-                // From EX/MEM (exPrev)
-                if (exPrev.writesRd && exPrev.rd == idPrev.rs2 &&
-                    exPrev.doesArithLogic) {
-                    idPrev.op2Val = exPrev.arithResult;
-                }
-                // From MEM (memPrev)
-                if (memPrev.writesRd && memPrev.rd == idPrev.rs2) {
-                    if (memPrev.readsMem) {
-                        idPrev.op2Val = memPrev.memResult;
-                    } else if (memPrev.doesArithLogic) {
-                        idPrev.op2Val = memPrev.arithResult;
-                    }
-                }        
-            }
-
-            pipelineInfo.exInst = simulator->simEX(idPrev);
-        }
-
-        // Do FORWARDING between prevMem and Ex and/or prevEx and Ex (done)
-        
-    // ID
-    
-        Simulator::Instruction newIdInst;
-
-        if(StallID){
-            // do not advance IF inst to ID
-            newIdInst = idPrev;
-        } else {
-            // normal
-            newIdInst = simulator->simID(ifPrev);
-        }
-
-        // DO Forwarding to ID for braches
-        bool newIsBranch = (newIdInst.opcode == OP_BRANCH) || 
-                           (newIdInst.opcode == OP_JAL)    ||
-                           (newIdInst.opcode == OP_JALR);
-
-        if (newIsBranch) {
-            // rs1 forwarding for branch
-            if (newIdInst.readsRs1 && newIdInst.rs1 != 0) {
-                // From EX/MEM  (exPrev)
-                if (exPrev.writesRd && exPrev.rd == newIdInst.rs1 &&
-                    exPrev.doesArithLogic) {
-                    newIdInst.op1Val = exPrev.arithResult;
-                }
-                // From MEM  (memPrev)
-                if (memPrev.writesRd && memPrev.rd == newIdInst.rs1) {
-                    if (memPrev.readsMem) {
-                        newIdInst.op1Val = memPrev.memResult;
-                    } else if (memPrev.doesArithLogic) {
-                        newIdInst.op1Val = memPrev.arithResult;
-                    }
-                }
-            }
-
-            // rs2 forwarding for branch
-            if (newIdInst.readsRs2 && newIdInst.rs2 != 0) {
-                // From EX/MEM  (exPrev)
-                if (exPrev.writesRd && exPrev.rd == newIdInst.rs2 &&
-                    exPrev.doesArithLogic) {
-                    newIdInst.op2Val = exPrev.arithResult;
-                }
-                // From MEM (memPrev)
-                if (memPrev.writesRd && memPrev.rd == newIdInst.rs2) {
-                    if (memPrev.readsMem) {
-                        newIdInst.op2Val = memPrev.memResult;
-                    } else if (memPrev.doesArithLogic) {
-                        newIdInst.op2Val = memPrev.arithResult;
-                    }
-                }
-
-            }
-        }
-
-        bool taken = isBranch && (newIdInst.nextPC != newIdInst.PC + 4);
-
-    //  ID + IF
-        if(!StallID) {
-            // if branch taken, then flush the if inst and move PC to 
-            // the address given by the branch
-            if(taken){
-                pipelineInfo.idInst = nop(SQUASHED);
-                PC = newIdInst.nextPC;
-                /*
-                NOTE
-                I am assuming here that if there is a branch which will change the
-                PC then we are to abort the stall cycles for the 
-                */
-                iCacheStallCycles = 0;
-                pipelineInfo.ifInst = simulator->simIF(PC);
-            }
-            else if (iCacheStallCycles >0) {
-                // not taken, then just move on with PC + 4
-                pipelineInfo.idInst = newIdInst;
-                pipelineInfo.ifInst = nop(IDLE);
-                iCacheStallCycles--;
-            }
-            else if(!iCache->access()) {
-                pipelineInfo.idInst = newIdInst;
-                pipelineInfo.ifInst = nop(IDLE);
-                iCacheStallCycles = iCache->config->missLatency;
-            } else {
-                pipelineInfo.idInst = newIdInst;
-                pipelineInfo.ifInst = simulator->simIF(PC);
-            }
-
-        }
+        // Case: dCache hit OR memory not accessed
         else {
+            
+            bool StallID    = false;
+            bool BubbleEx   = false;
 
-        }
-        // update pipeline's ID inst
-        pipelineInfo.idInst = newIdInst; 
+            // Hazard Detection
+            
+            bool isBranch = (idPrev.opcode == OP_BRANCH) || 
+                            (idPrev.opcode == OP_JAL) ||
+                            (idPrev.opcode == OP_JALR);
 
-        // DO exceptions
-        
+            // Handle load-use stalls
+            BubbleEx = (idPrev.rs1 == exPrev.rd || idPrev.rs2 == exPrev.rd) && exPrev.readsMem && exPrev.writesRd
+                        && exPrev.rd != 0 && !idPrevisBranch;
 
-// Previous Code
-        /* // Handle load-use stalls
-        if((pipelineInfo.idInst.rs1 == pipelineInfo.memInst.rd ||
-            pipelineInfo.idInst.rs2 == pipelineInfo.memInst.rd) && 
-            pipelineInfo.memInst.readsMem){
-            pipelineInfo.exInst = nop(BUBBLE);
+            // Ex
 
-            // handle memory stage forwarding (reduce load-use stall to 1 from 2)
-            if(pipelineInfo.idInst.rs1 == pipelineInfo.memInst.rd)
-                pipelineInfo.idInst.regData[pipelineInfo.idInst.rs1] = pipelineInfo.memInst.regData[pipelineInfo.memIsnt.rd];
-            else if(pipelineInfo.idInst.rs2 == pipelineInfo.memInst.rd)
-                pipelineInfo.idInst.regData[pipelineInfo.idInst.rs2] = pipelineInfo.memInst.regData[pipelineInfo.memIsnt.rd];
+            // Case: Load-Use Stall
+            if(BubbleEx) {
 
-        } else {
+                // Handle forwarding from the memory register to the ID stage
+                pipelineInfo.idInst.op1Val = (pipelineInfo.memInst.writesRd && pipelineInfo.memInst.rd 
+                                    == idPrev.rs1 && memPrev.readsMem) ? pipelineInfo.memInst.memResult;
+                pipelineInfo.idInst.op2Val = (pipelineInfo.memInst.writesRd && pipelineInfo.memInst.rd 
+                                    == idPrev.rs2 && memPrev.readsMem) ? pipelineInfo.memInst.memResult;
 
-            // handle execute stage forwarding (no stall at all)
-            if(pipelineInfo.idInst.rs1 == pipelineInfo.exInst.rd)
-                pipelineInfo.idInst.regData[pipelineInfo.idInst.rs1] = pipelineInfo.exInst.regData[pipelineInfo.exInst.rd];
-            else if(pipelineInfo.idInst.rs2 == pipelineInfo.memInst.rd)
-                pipelineInfo.idInst.regData[pipelineInfo.idInst.rs2] = pipelineInfo.exInst.regData[pipelineInfo.exInst.rd];
+                // insert bubble
+                pipelineInfo.exInst = nop(BUBBLE);
+            } 
+            // Case: No Load-Use Stall
+            else {
+                // normal case
+                // Do FORWARDING between prevMem and Ex and/or prevEx and Ex
+                idPrev.op1Val = (exPrev.writesRd && exPrev.rd == idPrev.rs1 && exPrev.doesArithLogic) ? exPrev.arithResult;
+                idPrev.op2Val = (exPrev.writesRd && exPrev.rd == idPrev.rs2 && exPrev.doesArithLogic) ? exPrev.arithResult;
+                
+                /*
+                Making slight changes to forwarding to prevent mem from writing over ex
+                OLD CODE
 
-            pipelineInfo.exInst = simulator->simEX(pipelineInfo.idInst);
-            // handle branch stalls
-            if(pipelineInfo.exInst.nextPC != pipelineInfo.exInst.PC+4){
-                pipelineInfo.idInst = nop(SQUASHED);
+                // rs1 forwarding - these can be put in helper functions!!
+                if (idPrev.readsRs1 && idPrev.rs1 != 0) {
 
-                // handle branch forwarding
-                pipelineInfo.ifInst.PC = pipelineInfo.exInst.nextPC;
+                    // From EX/MEM (exPrev) - ALU result is ready here
+                    if (exPrev.writesRd && exPrev.rd == idPrev.rs1 &&
+                        exPrev.doesArithLogic) {
+                        idPrev.op1Val = exPrev.arithResult;
+                    }
+                    // From MEM  (memPrev) - load result is ready here
+                    if (memPrev.writesRd && memPrev.rd == idPrev.rs1) {
+                        if (memPrev.readsMem) {
+                            idPrev.op1Val = memPrev.memResult;
+                        } else if (memPrev.doesArithLogic) {
+                            idPrev.op1Val = memPrev.arithResult;
+                        }
+                    }
+                    // From WB stage (just wrote back this cycle)- SORRY,
+                    // I deleted it, we dont need it
+                
+                }
+                // rs2 forwarding
+                if (idPrev.readsRs2 && idPrev.rs2 != 0) {
+                    // From EX/MEM (exPrev)
+                    if (exPrev.writesRd && exPrev.rd == idPrev.rs2 &&
+                        exPrev.doesArithLogic) {
+                        idPrev.op2Val = exPrev.arithResult;
+                    }
+                    // From MEM (memPrev)
+                    if (memPrev.writesRd && memPrev.rd == idPrev.rs2) {
+                        if (memPrev.readsMem) {
+                            idPrev.op2Val = memPrev.memResult;
+                        } else if (memPrev.doesArithLogic) {
+                            idPrev.op2Val = memPrev.arithResult;
+                        }
+                    }        
+                }
+                */
+                pipelineInfo.exInst = simulator->simEX(idPrev);
+                
+                // ID
+            
+                //  ID + IF
+
+                // Handle arith-branch
+                // Question: Should we change to check for BEQ/BNE/ Arith branches?      
+                StallID = isBranch && (idPrev.nextPC != idPrev.PC + 4);
+
+                // Case: Branch is taken
+                if(StallID){
+                    loadStallCount++;
+                    pipelineInfo.idInst = nop(SQUASHED);
+                    PC = idPrev.nextPC;
+                    /*
+                    NOTE
+                    I am assuming here that if there is a branch which will change the
+                    PC then we are to abort the stall cycles for the 
+                    */
+                    iCacheStallCycles = 0;
+                    pipelineInfo.ifInst = simulator->simIF(PC);
+                }
+                // Case: Branch is not taken OR no branch
+                else {
+                    
+                    pipelineInfo.idInst = simulator->simID(ifPrev);
+                    PC = (!pipelineInfo.idInst.isLegal) 0x8000:
+                    pipelineInfo.idInst.nextPC;
+                    // Case: Illegal Instruction in ID
+                    if(!pipelineInfo.idInst.isLegal){
+                        pipelineInfo.idInst = nop(SQUASHED);
+                        // 0x8000 is the error handling address
+                        pipelineInfo.ifInst = simulator->simIF(0x8000);
+                    }
+                    // Case: Still Stalling from previous iCache miss
+                    else if (iCacheStallCycles > 0) {
+                        // not taken, then just move on with PC + 4
+                        pipelineInfo.ifInst = nop(IDLE);
+                        iCacheStallCycles--;
+                    }
+                    // Case: iCache miss
+                    else if(!iCache->access(PC, CACHE_READ)) {
+                        pipelineInfo.ifInst = nop(IDLE);
+                        iCacheStallCycles = iCache->config->missLatency;
+                    } 
+                    // Case: iCache hit and legal instruction
+                    else {
+                        pipelineInfo.ifInst = simulator->simIF(PC);
+                    }
+                }
+
             }
-            else 
-                pipelineInfo.idInst = simulator->simID(pipelineInfo.ifInst);
-
-            pipelineInfo.ifInst = simulator->simIF(PC);
-        }*/
-
-        // Stall Checking Conditions
-        // To be implemented
-        // Load-Use: 1 cycle
-        // Arithmatic Branch: 1 cycle
-        // Load-branch: 2 cycle
+        }
 
         // WB Check for halt instruction
         if (pipelineInfo.wbInst.isHalt) {
@@ -357,7 +276,8 @@ Status runTillHalt() {
 // dump the state of the simulator
 Status finalizeSimulator() {
     simulator->dumpRegMem(output);
-    SimulationStats stats{simulator->getDin(),  cycleCount, 0, 0, 0, 0, 0};  // TODO incomplete implementation
+    SimulationStats stats{simulator->getDin(),  cycleCount, iCache->getHits(), 
+                iCache->getMisses(), dCache->getHits(), dCache->getMisses(), loadStallCount}; 
     dumpSimStats(stats, output);
     return SUCCESS;
 }
