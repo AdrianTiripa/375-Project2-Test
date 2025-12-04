c9d28b51253d8701a7c3fff60575ad0bd43d8bd8
ctganter <cg6149@princeton.edu>
2025-12-03 23:16:27 -0500

    bug fix

--- project2/src/cache.cpp
+++ project2/src/cache.cpp
@@ -40,10 +40,6 @@ Cache::Cache(CacheConfig configParam, CacheDataType cacheType) : config(configPa
 
 }
 
-void Cache::invalidate(uint64_t address){
-    
-}
-
 // Access method definition
 bool Cache::access(uint64_t address, CacheOperation readWrite) {
 
