1b05d4b959bebcc985ed804e65fcf952a14ed4b0
AdrianTiripa <at9278@princeton.edu>
2025-12-07 16:22:10 -0500

    try cache as well

--- project2/src/cache.cpp
+++ project2/src/cache.cpp
@@ -4,6 +4,7 @@
 #include "cache.h"
 #include <random>
 #include <vector>
+#include <cmath>    // for log2
 
 using namespace std;
 
@@ -17,107 +18,104 @@ Cache::Cache(CacheConfig configParam, CacheDataType cacheType) : config(configPa
     // For instance, if you had cache tables or other structures, initialize them here
     
 
-    hits=0;
-    misses=0;
-    type = cacheType;
-    time=0;
+    hits  = 0;
+    misses = 0;
+    type  = cacheType;
+    time  = 0;
 
     // Initialize cache structures
-    numBlocks = config.cacheSize/config.blockSize;
-    numSets = numBlocks/config.ways;
+    numBlocks = config.cacheSize / config.blockSize;
+    numSets   = numBlocks / config.ways;
     
-
     // cacheSize/blockSize gives number of blocks
     // (cacheSize/blockSize)/ways gives number of sets
     // configParam.ways gives number of blocks per set
     cacheArray.assign(numSets, vector<uint64_t>(config.ways, 0));
 
     // follows from above, every entry is set to false initially
-    validBits.assign(numSets,vector<bool>(config.ways, false));
+    validBits.assign(numSets, vector<bool>(config.ways, false));
 
     // initialize LRU counters to 0, there is one counter per set
     LRUCounter.assign(numSets, vector<uint64_t>(config.ways, 0));
-
 }
 
 // Access method definition
 bool Cache::access(uint64_t address, CacheOperation readWrite) {
 
-    // find tag, index, block offset sizes
-    uint64_t blockOffsetBits = log2(config.blockSize);
-    uint64_t indexBits = log2(numSets); // not sure of the signature
-    uint64_t tagBits = 64 - blockOffsetBits - indexBits;
+    // find tag, index, block offset sizes (sizes are powers of two)
+    uint64_t blockOffsetBits = static_cast<uint64_t>(std::log2(config.blockSize));
+    uint64_t indexBits       = static_cast<uint64_t>(std::log2(numSets));
+
+    uint64_t blockOffsetMask = (blockOffsetBits == 0) ? 0ULL : ((1ULL << blockOffsetBits) - 1ULL);
+    uint64_t indexMask       = (indexBits == 0) ? 0ULL : ((1ULL << indexBits) - 1ULL);
 
     // extract tag, index, block offset from address
-    uint64_t blockOffset = address & ((1 << blockOffsetBits) - 1);
-    uint64_t index = (address >> blockOffsetBits) & ((1 << indexBits) - 1);
-    uint64_t tag = address >> (blockOffsetBits + indexBits);
+    uint64_t blockOffset = address & blockOffsetMask;
+    (void)blockOffset; // not used but kept for clarity / future debugging
+
+    uint64_t index = (indexBits == 0) ? 0ULL : ((address >> blockOffsetBits) & indexMask);
+    uint64_t tag   = address >> (blockOffsetBits + indexBits);
 
     // look for hit in the corresponding set
-    bool hit = false;
-    for(uint64_t i = 0; i < config.ways; i++) {
-        if(validBits[index][i] && cacheArray[index][i] == tag) {
-            hit = true;
+    for (uint64_t way = 0; way < config.ways; ++way) {
+        if (validBits[index][way] && cacheArray[index][way] == tag) {
             hits++;
             // update LRU counter
-            LRUCounter[index][i] = ++time;
-            return hit;
+            LRUCounter[index][way] = ++time;
+            return true;
         }
     }
 
-    // on miss, increase miss count and replace LRU block
-    if(!hit){
-        misses++;
+    // miss: increase miss count and replace LRU block
+    misses++;
 
-        // look for an invalid block first
-        for(int i=0; i<config.ways; i++){
-            if(!validBits[index][i]){
-                cacheArray[index][i] = tag;
-                validBits[index][i] = true;
-                LRUCounter[index][i] = ++time;
-                return hit;
-            }
+    // look for an invalid block first
+    for (uint64_t way = 0; way < config.ways; ++way) {
+        if (!validBits[index][way]) {
+            cacheArray[index][way] = tag;
+            validBits[index][way]  = true;
+            LRUCounter[index][way] = ++time;
+            return false;
         }
-
-        // if all blocks are valid, replace LRU block
-        uint64_t leastTime=LRUCounter[index][0];
-        uint64_t LRUBlock=0;
-
-        // find LRU block
-        for(int i=0; i<config.ways; i++){
-            if(LRUCounter[index][i]<leastTime){
-                leastTime = LRUCounter[index][i];
-                LRUBlock = i;
-            }
-        }
-        cacheArray[index][LRUBlock] = tag;
-        validBits[index][LRUBlock] = true;
-        LRUCounter[index][LRUBlock] = ++time;
-        return hit;
     }
 
-    // For simplicity, we're using a random boolean to simulate cache hit/miss
-    /*bool hit = distribution(generator) < 0.20;  // random 20% hit for a strange cache
-    hits += hit;
-    misses += !hit;*/
-    return hit;
+    // if all blocks are valid, replace LRU block
+    uint64_t LRUBlock   = 0;
+    uint64_t leastTime  = LRUCounter[index][0];
+
+    // find LRU block
+    for (uint64_t way = 1; way < config.ways; ++way) {
+        if (LRUCounter[index][way] < leastTime) {
+            leastTime = LRUCounter[index][way];
+            LRUBlock  = way;
+        }
+    }
+
+    cacheArray[index][LRUBlock] = tag;
+    validBits[index][LRUBlock]  = true;
+    LRUCounter[index][LRUBlock] = ++time;
+
+    return false;
 }
-
 
 void Cache::invalidate(uint64_t address){
     // find tag, index, block offset sizes
-    uint64_t blockOffsetBits = log2(config.blockSize);
-    uint64_t indexBits = log2(numSets); // not sure of the signature
-    uint64_t tagBits = 64 - blockOffsetBits - indexBits;
+    uint64_t blockOffsetBits = static_cast<uint64_t>(std::log2(config.blockSize));
+    uint64_t indexBits       = static_cast<uint64_t>(std::log2(numSets));
+
+    uint64_t blockOffsetMask = (blockOffsetBits == 0) ? 0ULL : ((1ULL << blockOffsetBits) - 1ULL);
+    uint64_t indexMask       = (indexBits == 0) ? 0ULL : ((1ULL << indexBits) - 1ULL);
 
     // extract tag, index, block offset from address
-    uint64_t blockOffset = address & ((1 << blockOffsetBits) - 1);
-    uint64_t index = (address >> blockOffsetBits) & ((1 << indexBits) - 1);
-    uint64_t tag = address >> (blockOffsetBits + indexBits);
+    uint64_t blockOffset = address & blockOffsetMask;
+    (void)blockOffset; // not used
 
-    for(int i=0; i<config.ways; i++){
-        if(validBits[index][i] && cacheArray[index][i] == tag){
-            validBits[index][i] = false; // invalidate the block
+    uint64_t index = (indexBits == 0) ? 0ULL : ((address >> blockOffsetBits) & indexMask);
+    uint64_t tag   = address >> (blockOffsetBits + indexBits);
+
+    for (uint64_t way = 0; way < config.ways; ++way) {
+        if (validBits[index][way] && cacheArray[index][way] == tag) {
+            validBits[index][way] = false; // invalidate the block
             break;
         }
     }
@@ -133,7 +131,7 @@ Status Cache::dump(const std::string& base_output_name) {
         cache_out << "Cache Configuration:" << std::endl;
         cache_out << "Size: " << config.cacheSize << " bytes" << std::endl;
         cache_out << "Block Size: " << config.blockSize << " bytes" << std::endl;
-        cache_out << "Ways: " << (config.ways == 1) << std::endl;
+        cache_out << "Ways: " << config.ways << std::endl;
         cache_out << "Miss Latency: " << config.missLatency << " cycles" << std::endl;
         cache_out << "---------------------" << endl;
         cache_out << "End Register Values" << endl;
